d82282c02e10311ee407f95e9a0376ed72a35f9f
Yeison Torrado <yeiman_4@hotmail.com>
2018-07-11 17:41:45 -0500

    evitar cache

--- app/application/views/inicio/head.php
+++ app/application/views/inicio/head.php
@@ -10,25 +10,25 @@
 	<![endif]-->
         
 		
-	<link href="<?php echo base_url("assets/fontastic/styles.css") ?>" rel="stylesheet">
-	<link href="<?php echo base_url("assets/bootstrap/css/bootstrap.css") ?>" rel="stylesheet" />
-	<link href="<?php echo base_url("assets/estilo-gestion.css") ?>" rel="stylesheet" />
-        <link href="<?php echo base_url("assets/estilo-principal.css") ?>" rel="stylesheet" />
+	<link href="<?php echo base_url("assets/fontastic/styles.css") ?>?v=<?php echo rand(); ?>" rel="stylesheet">
+	<link href="<?php echo base_url("assets/bootstrap/css/bootstrap.css") ?>?v=<?php echo rand(); ?>" rel="stylesheet" />
+	<link href="<?php echo base_url("assets/estilo-gestion.css") ?>?v=<?php echo rand(); ?>" rel="stylesheet" />
+        <link href="<?php echo base_url("assets/estilo-principal.css") ?>?v=<?php echo rand(); ?>" rel="stylesheet" />
 
         <!--  ============ plugins =========== -->
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-        <script src="<?php echo base_url("assets/bootstrap/js/bootstrap.min.js") ?>" rel="stylesheet"></script>
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js?v=<?php echo rand(); ?>" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js?v=<?php echo rand(); ?>" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+        <script src="<?php echo base_url("assets/bootstrap/js/bootstrap.min.js") ?>?v=<?php echo rand(); ?>" rel="stylesheet"></script>
 
         <!-- inicio AJAX UPLOADFILE PLUGIN -->
-        <link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/js/jquery-uploadfile/uploadfile.css") ?>">
-        <script type="text/javascript" src="<?php echo base_url("assets/js/jquery-uploadfile/jquery.form.js"); ?>"></script>
-        <script type="text/javascript" src="<?php echo base_url("assets/js/jquery-uploadfile/jquery.uploadfile.min.js"); ?>"></script>
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/js/jquery-uploadfile/uploadfile.css") ?>?v=<?php echo rand(); ?>">
+        <script type="text/javascript" src="<?php echo base_url("assets/js/jquery-uploadfile/jquery.form.js"); ?>?v=<?php echo rand(); ?>"></script>
+        <script type="text/javascript" src="<?php echo base_url("assets/js/jquery-uploadfile/jquery.uploadfile.min.js"); ?>?v=<?php echo rand(); ?>"></script>
 	<!-- fin AJAX UPLOADFILE PLUGIN -->
 
         <!-- Angular -->
-        <script type="text/javascript" src="<?php echo base_url("assets/js/angular.min.js"); ?>"></script>
-        <script type="text/javascript" src="<?php echo base_url("assets/js/app.js"); ?>"></script>
+        <script type="text/javascript" src="<?php echo base_url("assets/js/angular.min.js"); ?>?v=<?php echo rand(); ?>"></script>
+        <script type="text/javascript" src="<?php echo base_url("assets/js/app.js"); ?>?v=<?php echo rand(); ?>"></script>
 		
 	<title>Termotecnica: Plataforma de gestion de usuarios.</title>
 </head>
