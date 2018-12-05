30af75ec157b3b541b05b4cc1416f2e771de47a0
Yeison <tristan,herth@gmail.com>
2018-12-05 09:56:34 -0500

    Mod 1.9.1.4

--- app/application/views/administracion/usuarios/formRolUser.php
+++ app/application/views/administracion/usuarios/formRolUser.php
@@ -16,7 +16,7 @@
 					<select ng-model="formRol" ng-options="r as r.nombre_rol for r in roles">
 					</select>
 
-					<button ng-click="asignarRol(myUser, formRol, '<?= site_url('usuario/save'); ?>')">Asignaro rol</button>
+					<button ng-click="asignarRol(myUser, formRol, '<?= site_url('usuario/save'); ?>', '#formUserRol')">Asignaro rol</button>
 				</fieldset>
 			</div>
 			<div class="modal-footer">
--- assets/js/app.js
+++ assets/js/app.js
@@ -326,15 +326,17 @@ app.controller("usuarios",function($scope, $http, $timeout){
 		});
 	}
 
-	$scope.asignarRol = function(user, rol, lnk){
-		let data = angular.copy(user);
-		data.idrol = rol.idrol;
-		data.nombre_rol = rol.nombre_rol;
-		$scope.peticion(lnk, data, function(resp){
-			user.idrol = rol.idrol;
-			user.nombre_rol = rol.nombre_rol;
-		});
-		$(tag).modal("toggle");
+	$scope.asignarRol = function(user, rol, lnk, tag){
+		if( confirm("¿Confirma el cambio de rol?") ){
+			let data = angular.copy(user);
+			data.idrol = rol.idrol;
+			data.nombre_rol = rol.nombre_rol;
+			$scope.peticion(lnk, data, function(resp){
+				user.idrol = rol.idrol;
+				user.nombre_rol = rol.nombre_rol;
+				$(tag).modal("toggle");
+			});
+		}
 	}
 
 
